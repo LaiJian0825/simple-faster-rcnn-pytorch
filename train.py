9632cfdba1360d23d6da4ab78b05a104fb9e7060
laijian0825 <laijian2@gemdale.com>
2022-07-26 14:25:01 +0800

    Update train.py

--- train.py
+++ train.py
@@ -51,13 +51,13 @@ def train(**kwargs):
     dataset = Dataset(opt)
     print('load data')
     dataloader = data_.DataLoader(dataset, \
-                                  batch_size=2, \
+                                  batch_size=1, \
                                   shuffle=True, \
                                   # pin_memory=True,
                                   num_workers=opt.num_workers)
     testset = TestDataset(opt)
     test_dataloader = data_.DataLoader(testset,
-                                       batch_size=2,
+                                       batch_size=1,
                                        num_workers=opt.test_num_workers,
                                        shuffle=False, \
                                        pin_memory=True
