d5c260538bafac5149b2ec35284fffad5763d530
laijian0825 <laijian2@gemdale.com>
2022-07-26 15:50:25 +0800

    visdom ip改成0.0.0.0

--- utils/vis_tool.py
+++ utils/vis_tool.py
@@ -175,7 +175,7 @@ class Visualizer(object):
     """
 
     def __init__(self, env='default', **kwargs):
-        self.vis = visdom.Visdom('localhost',env=env, use_incoming_socket=False, **kwargs)
+        self.vis = visdom.Visdom('0.0.0.0',env=env, use_incoming_socket=False, **kwargs)
         self._vis_kw = kwargs
 
         # e.g.('loss',23) the 23th value of loss
