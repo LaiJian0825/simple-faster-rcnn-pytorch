6bd76eacc1f16fdc32eb1e044f435d66ebe1a5d7
laijian0825 <laijian2@gemdale.com>
2022-07-28 15:11:52 +0800

    使用Ali dataset

--- data/voc_dataset.py
+++ data/voc_dataset.py
@@ -138,8 +138,8 @@ class VOCBboxDataset:
         difficult = np.array(difficult, dtype=np.bool).astype(np.uint8)  # PyTorch don't support np.bool
 
         # Load a image
-        # img_file = os.path.join(self.data_dir, 'JPEGImages', id_ + '.png') Ali
-        img_file = os.path.join(self.data_dir, 'JPEGImages', id_ + '.jpg')
+        img_file = os.path.join(self.data_dir, 'JPEGImages', id_ + '.png')
+        # img_file = os.path.join(self.data_dir, 'JPEGImages', id_ + '.jpg')
         img = read_image(img_file, color=True)
 
         # if self.return_difficult:
