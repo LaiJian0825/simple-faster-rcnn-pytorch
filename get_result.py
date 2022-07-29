c26c4cb51e6c486f97a87d147e42b97beaf0cb8e
laijian0825 <laijian2@gemdale.com>
2022-07-29 15:08:48 +0800

    更改路径

--- get_result.py
+++ get_result.py
@@ -1,3 +1,4 @@
+import glob
 import os
 import torch as t
 from utils.config import opt
@@ -7,9 +8,19 @@ from data.util import read_image
 from utils.vis_tool import vis_bbox
 from utils import array_tool as at
 
-image_path = r'/media/dataset/VOCcad/JPEGImages'
-result_path = r'/root/projects/simple-faster-rcnn-pytorch/result'
-for img in read_image(image_path):
+# import os
+# import glob
+# WSI_MASK_PATH = 'E:\\contest\\insect\\insect\\JPEGImages\\test'#存放图片的文件夹路径
+# paths = glob.glob(os.path.join(WSI_MASK_PATH, '*.jpeg'))
+# print(paths)
+# paths.sort()
+# print(paths)
+
+image_path = r'/media/dataset/VOCcad/JPEGImages/'
+imgs = glob.glob(os.path.join(image_path,'*.png'))
+result_path = r'/root/projects/simple-faster-rcnn-pytorch/result/'
+for img in imgs:
+    id = img.split('/')[-1]
     id_image = t.from_numpy(img)[None]
     faster_rcnn = FasterRCNNVGG16()
     trainer = FasterRCNNTrainer(faster_rcnn).cuda()
@@ -19,7 +30,7 @@ for img in read_image(image_path):
     # id_list_file = os.path.join(
     #     data_dir, 'ImageSets/Main/{0}.txt'.format(split))
     # file = open(result_path, 'w')
-    file = open(os.path.join(result_path, '{}.txt').format(img))
+    file = open(os.path.join(result_path, '{}.txt').format(id))
     file.write(_bboxes)
     file.write(_labels)
     file.write(_scores)
