dd4bd24cee47e00f98c4606529bfd8543183d758
laijian0825 <laijian2@gemdale.com>
2022-08-01 09:47:57 +0800

    预测/dev/validation/下的图片

--- get_result.py
+++ get_result.py
@@ -16,24 +16,25 @@ from utils import array_tool as at
 # paths.sort()
 # print(paths)
 
-image_path = r'/media/dataset/VOCcad/JPEGImages/'
+image_path = '/dev/validation/'
 imgs = glob.glob(os.path.join(image_path,'*.png'))
-result_path = r'/root/projects/simple-faster-rcnn-pytorch/result/'
+result_path = 'result/'
 for img in imgs:
     id = img.split('/')[-1]
     img = read_image(img, color=True)
     id_image = t.from_numpy(img)[None]
     faster_rcnn = FasterRCNNVGG16()
     trainer = FasterRCNNTrainer(faster_rcnn).cuda()
-    trainer.load('checkpoints/fasterrcnn_07291035_0.6486721503704074')
-    opt.caffe_pretrain = False # this model was trained from torchvision-pretrained model
+    trainer.load('checkpoints/fasterrcnn_07291235_0.689377591224362')
+    opt.caffe_pretrain = True  # this model was trained from torchvision-pretrained model
     _bboxes, _labels, _scores = trainer.faster_rcnn.predict(img, visualize=True)
     # id_list_file = os.path.join(
     #     data_dir, 'ImageSets/Main/{0}.txt'.format(split))
     # file = open(result_path, 'w')
     file = open(os.path.join(result_path, '{}.txt').format(id))
-    file.write(_bboxes)
-    file.write(_labels)
-    file.write(_scores)
+    file.write(str(_labels[0][0]) + ' ')
+    file.write(str(_bboxes[0][0]) + ' ')
+    file.write(str(_scores[0][0]) + ' ')
+    file.close()
 
 
