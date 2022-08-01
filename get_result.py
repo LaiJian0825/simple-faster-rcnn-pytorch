1136038e43a49149da79060dfbf20341a8b38cb0
laijian0825 <laijian2@gemdale.com>
2022-08-01 16:14:03 +0800

    预测/media/dataset/test/ali_test_nanning/ 图片

--- get_result.py
+++ get_result.py
@@ -8,13 +8,6 @@ from data.util import read_image
 from utils.vis_tool import vis_bbox
 from utils import array_tool as at
 
-# import os
-# import glob
-# WSI_MASK_PATH = 'E:\\contest\\insect\\insect\\JPEGImages\\test'#存放图片的文件夹路径
-# paths = glob.glob(os.path.join(WSI_MASK_PATH, '*.jpeg'))
-# print(paths)
-# paths.sort()
-# print(paths)
 
 # image_path = '/dev/validation/'
 image_path = '/media/dataset/test/ali_test_nanning/'
@@ -31,7 +24,6 @@ for img in imgs:
     trainer.load('checkpoints/fasterrcnn_07291035_0.6486721503704074')
     opt.caffe_pretrain = False  # this model was trained from torchvision-pretrained model
     _bboxes, _labels, _scores = trainer.faster_rcnn.predict(id_image, visualize=True)
-
     # print('_bboxes:{}'.format(_bboxes))
     # print('_labels:{}'.format(_labels))
     # print('_scores:{}'.format(_scores))
